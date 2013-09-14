379816745936f750609308a0de6b8a9cd9156b0c
Ben <benbenben001@gmail.com>
2013-09-14 12:28:38 +0100

    A bit of a change

---
+++ src/main/java/net/lazlecraft/bungeewhitelist/BhiteList.java
@@ -0,0 +1,82 @@
+package net.lazlecraft.bungeewhitelist;
+
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.plugin.Command;
+
+import java.util.List;
+
+public class BhiteList extends Command {
+
+    private final BungeeWhitelist whitelist;
+
+    public BhiteList(BungeeWhitelist whitelist) {
+        super("bhitelist", "bhitelist.edit");
+        this.whitelist = whitelist;
+    }
+
+    @Override
+    public void execute(CommandSender commandSender, String[] strings) {
+        // enable/disable
+        if(strings.length == 1) {
+            if(strings[0].equalsIgnoreCase("enable")) {
+                whitelist.getConfig().set("Enabled", true);
+                whitelist.saveConfig();
+                commandSender.sendMessage(BungeeWhitelist.PREFIX+"Enabled");
+            } else if(strings[0].equalsIgnoreCase("disable")) {
+                whitelist.getConfig().set("Enabled", false);
+                whitelist.saveConfig();
+                commandSender.sendMessage(BungeeWhitelist.PREFIX+"Disabled");
+            }
+        } else if(strings.length == 2) {
+            // add/remove
+            if(strings[0].equalsIgnoreCase("add")) {
+                String toAdd = strings[1];
+                List<String> players = whitelist.getConfig().getStringList("Whitelist");
+                if(containsIgnoreCase(players, toAdd)) {
+                    commandSender.sendMessage(BungeeWhitelist.PREFIX+toAdd+" is already bhitelisted");
+                } else {
+                    players.add(toAdd);
+                    whitelist.getConfig().set("Whitelist", players);
+                    whitelist.saveConfig();
+                }
+            } else if(strings[0].equalsIgnoreCase("remove")) {
+                String toRemove = strings[1];
+                List<String> players = whitelist.getConfig().getStringList("Whitelist");
+                if(containsIgnoreCase(players, toRemove)) {
+                    removeIgnoreCase(players, toRemove);
+                    whitelist.getConfig().set("Whitelist", players);
+                    whitelist.saveConfig();
+                } else {
+                    commandSender.sendMessage(BungeeWhitelist.PREFIX+toRemove+" is not bhitelisted");
+                }
+            }
+        }
+    }
+
+    public static boolean containsIgnoreCase(List<String> list, String value) {
+        for(String s : list) {
+            if(s.equalsIgnoreCase(value)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void removeIgnoreCase(List<String> list, String value) {
+        int key = -1;
+        for(int i=0; i<list.size(); i++) {
+            String s = list.get(i);
+            if(s.equalsIgnoreCase(value)) {
+                key = i;
+            }
+        }
+        if(key != -1) {
+            list.remove(key);
+        }
+    }
+
+    public void error(CommandSender commandSender) {
+        commandSender.sendMessage(BungeeWhitelist.PREFIX+"Usage: /bhitelist enable|disable|add|remove [player]");
+    }
+}
--- src/main/java/net/lazlecraft/bungeewhitelist/BungeeWhitelist.java
+++ src/main/java/net/lazlecraft/bungeewhitelist/BungeeWhitelist.java
@@ -8,33 +8,30 @@ import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
 public class BungeeWhitelist extends ConfigurablePlugin implements Listener {
-	
-	public String kickmsg;
-	
-	public void onEnable() {
-		registerFeature();
-		kickmsg = this.getConfig().getString("KickMessage");
-	}
-	
-	public void registerFeature() {
-		if (ProxyServer.getInstance().getConfigurationAdapter().getBoolean("online_mode",true)) {
-			ProxyServer.getInstance().getPluginManager().registerListener(this, this);
-			this.getConfig().options().copyDefaults(true);
-			this.saveConfig();
-		}
-		else {
-			System.out.println(ChatColor.GREEN + "A message from LaxWasHere");
-			System.out.println("56 47 68 70 63 79 42 77 62 48 56 6e 61 57 34 67 5a 47 39 6c 63 79 42 75 62 33 51 67 63 33 56 77 63 47 39 79 64 43 42 6a 63 6d 46 6a 61 32 56 6b 49 48 4e 6c 63 6e 5a 6c 63 6e 4d 75 49 45 6c 6d 49 48 6c 76 64 53 42 33 61 58 4e 6f 49 48 52 76 49 48 56 7a 5a 53 42 30 61 47 6c 7a 49 48 42 73 64 57 64 70 62 69 77 67 59 6e 56 35 49 48 52 6f 5a 53 42 6d 64 57 4e 72 61 57 35 6e 49 47 64 68 62 57 55 67 62 33 49 67 59 32 39 6b 5a 53 42 76 62 6d 55 67 65 57 39 31 63 6e 4e 6c 62 47 59 75 49 46 6c 76 64 53 42 68 63 6d 55 67 59 57 34 67 59 57 4a 76 62 57 6c 75 59 58 52 70 62 32 34 67 64 47 38 67 64 47 68 6c 49 47 31 70 62 6d 56 6a 63 6d 46 6d 64 43 42 6a 62 32 31 74 64 57 35 70 64 48 6b 75");
-			ProxyServer.getInstance().stop();
-		}
-	}
-	
-	@EventHandler
-	public void onLogin(LoginEvent ev) {
-		String p = ev.getConnection().getName();
-		if (!getConfig().getStringList("Whitelist").contains(p)) {
-			ev.setCancelled(true);
-			ev.setCancelReason(ChatColor.translateAlternateColorCodes('&', kickmsg));
-		}
-	}
+
+    public String kickmsg;
+    public static final String PREFIX = ChatColor.GREEN+"["+ChatColor.AQUA+"BHITE"+ChatColor.GREEN+"] "+ChatColor.AQUA;
+
+    public void onEnable() {
+        registerFeature();
+        kickmsg = this.getConfig().getString("KickMessage");
+        ProxyServer.getInstance().getPluginManager().registerCommand(this, new BhiteList(this));
+    }
+
+    public void registerFeature() {
+        ProxyServer.getInstance().getPluginManager().registerListener(this, this);
+        this.getConfig().options().copyDefaults(true);
+        this.saveConfig();
+    }
+
+    @EventHandler
+    public void onLogin(LoginEvent ev) {
+        if(getConfig().getBoolean("Enabled")) {
+            String p = ev.getConnection().getName();
+            if (!BhiteList.containsIgnoreCase(getConfig().getStringList("Whitelist"), p)) {
+                ev.setCancelled(true);
+                ev.setCancelReason(ChatColor.translateAlternateColorCodes('&', kickmsg));
+            }
+        }
+    }
 }
